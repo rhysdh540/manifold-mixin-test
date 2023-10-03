7a09700a4b1231d34021bd9c011e7f928b1aa80c
rhysdh540 <rhys.dehaan@me.com>
2023-10-03 18:54:01 -0400

    fix... everything? no i didnt

--- src/main/java/dev/rdh/manifoldmixintest/Test.java
+++ src/main/java/dev/rdh/manifoldmixintest/Test.java
@@ -1,5 +1,6 @@
 package dev.rdh.manifoldmixintest;
 
+import dev.rdh.manifoldmixintest.mixin.MinecraftMixin;
 import dev.rdh.manifoldmixintest.mixin.PlacementInfoAccessor;
 import net.fabricmc.api.ModInitializer;
 
@@ -25,17 +26,15 @@ public class Test implements ModInitializer {
 	public void minecraft() {
 		final Minecraft mc = Minecraft.getInstance();
 
-		//get with mixin + interface injection (props doesn't work for some reason)
-		long mixinLastTime = mc.getLastTime();
-		l.info("mixin: {}", mixinLastTime);
+		//get with mixin + props
+		long mixinLastTime = ((MinecraftMixin)mc).lastTime;
 
 		//get with jailbreak (breaks when remapped)
-		long jailbreakLastTime = mc.jailbreak().lastTime++;
-		l.info("jailbreak: {}", jailbreakLastTime);
+		long jailbreakLastTime = mc.jailbreak().lastTime = 10;
 	}
 
 	/**
-	 * target: {@link PlacementInfo}
+	 * target: {@link PlacementInfo PlacementInfo}
 	 */
 	public void notMinecraft() {
 		PlacementInfo info = new PlacementInfo(null);
@@ -45,14 +44,14 @@ public class Test implements ModInitializer {
 
 		l.info("end2Extent: {}", ((PlacementInfoAccessor)info).end1Extent);
 
-		// get with jailbreak (works but mixin is better)
-		info.jailbreak().end1Extent++;
+		// get with jailbreak (works but really mixin is better)
+		info.jailbreak().end1Extent = 10;
 
 		// mixin + ext
 		info.setPos1(new BlockPos(0, 0, 0));
 		BlockPos pos1 = info.getPos1();
 
-		// mixin + props + ext (no work)
+		// mixin + props + ext
 		info.pos1 = new BlockPos(0, 0, 0);
 		BlockPos pos1_2 = info.pos1;
 	}
